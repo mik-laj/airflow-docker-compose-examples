28f92d3b8b6ffba69f06a899e69a3057a7c15e54
Kamil Breguła <kamilbregula@apache.org>
2021-05-30 22:46:21 +0200

    first commit

---
+++ render.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+import sys
+from argparse import ArgumentParser
+from pathlib import Path
+
+import httpx
+import jinja2
+import jsonschema
+import semver
+import yaml
+
+if __name__ not in ("__main__", "__mp_main__"):
+    raise SystemExit(
+        "This file is intended to be executed as an executable program. You cannot use it as a module."
+        f"To run this script, run the ./{__file__} command."
+    )
+
+TEMPLATE_DIR = Path(__file__).resolve().parent
+COMPOSE_SPEC_URL = (
+    "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"
+)
+
+
+def fail(message: str):
+    raise Exception(message)
+
+
+def render_jinja_template(template_name: str, **kwargs) -> str:
+    """Render Jinja template"""
+    template_loader = jinja2.FileSystemLoader(searchpath=TEMPLATE_DIR)
+    template_env = jinja2.Environment(
+        loader=template_loader, undefined=jinja2.StrictUndefined, lstrip_blocks=True, trim_blocks=True,
+    )
+    template: jinja2.Template = template_env.get_template(template_name)
+    content: str = template.render(**kwargs, fail=fail)
+    return content
+
+
+def validate_docker_compose(text_content: str) -> None:
+    content = yaml.safe_load(text_content)
+    response = httpx.get(COMPOSE_SPEC_URL)
+    response.raise_for_status()
+    schema = response.json()
+    jsonschema.validate(content, schema)
+
+
+def render_docker_compose(
+    *,
+    airflow_version: str,
+    executor: str,
+    db_backend: str
+) -> str:
+    text_content = render_jinja_template(
+        'docker-compose.yaml.jinja2',
+        airflow_version=airflow_version,
+        executor=executor,
+        db_backend=db_backend
+    )
+    try:
+        validate_docker_compose(text_content)
+    except:
+        print(text_content, file=sys.stderr)
+        raise
+    return text_content
+
+
+def get_parser() -> ArgumentParser:
+    parser = ArgumentParser(
+        prog=__file__,
+        description="Render"
+    )
+    parser.add_argument('--executor', choices=('CeleryExecutor', 'LocalExecutor'), required=True)
+    parser.add_argument('--airflow-version', type=semver.VersionInfo.parse, required=True)
+    parser.add_argument('--db-backend', choices=("postgres", "mysql"), default="postgres")
+
+    return parser
+
+
+def main() -> None:
+    parser = get_parser()
+    args = parser.parse_args()
+    if args.airflow_version < semver.VersionInfo.parse("2.1.0"):
+        print(f"Unsupported Airflow version [{args.airflow_version}]. At least version 2.1 is required.", file=sys.stderr)
+        sys.exit(1)
+    print(
+        render_docker_compose(
+            airflow_version=str(args.airflow_version),
+            executor=args.executor,
+            db_backend=args.db_backend
+        )
+    )
+
+main()
---
+++ test.sh
@@ -0,0 +1,168 @@
+#!/usr/bin/env bash
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+set -euo pipefail
+
+PROJECT_SOURCES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "${PROJECT_SOURCES}"
+
+tmp_output="$(mktemp)"
+# shellcheck disable=SC2064
+trap "rm -rf ${tmp_output}" EXIT
+tmp_output="/tmp/test.sh"
+echo "Temporary output file: ${tmp_output}"
+
+AIRFLOW_WWW_USER_USERNAME=${_AIRFLOW_WWW_USER_USERNAME:-airflow}
+AIRFLOW_WWW_USER_PASSWORD=${_AIRFLOW_WWW_USER_PASSWORD:-airflow}
+DAG_ID="example_bash_operator"
+DAG_RUN_ID="test_dag_run_id"
+
+cp "/Users/kamilbregula/devel/airflow/airflow/airflow/example_dags/example_bash_operator.py" "./dags"
+
+function wait_for_webserver {
+    echo "Waiting for webserver"
+    local countdown
+    countdown="50"
+
+    while true
+    do
+        set +e
+        local last_check_result
+        local res
+        last_check_result=$(
+            curl -X GET \
+                --header "Content-Type: application/json" \
+                -u "${AIRFLOW_WWW_USER_USERNAME}:${AIRFLOW_WWW_USER_PASSWORD}" \
+                "http://localhost:8080/api/v1/health" 2>&1)
+        res=$?
+        set -e
+        if [[ ${res} == 0 ]]; then
+            echo
+            break
+        else
+            echo -n "."
+            countdown=$((countdown-1))
+        fi
+        if [[ ${countdown} == 0 ]]; then
+            echo
+            echo "ERROR! Maximum number of retries (10) reached."
+            echo
+            echo "Last check result:"
+            echo "GET http://localhost:8080/api/v1/health"
+            echo "${last_check_result}"
+            echo
+            exit 1
+        else
+            sleep 1
+        fi
+    done
+}
+
+function fetch_dag_state {
+    curl -s -X GET \
+        --header "Content-Type: application/json" \
+        -u "${AIRFLOW_WWW_USER_USERNAME}:${AIRFLOW_WWW_USER_PASSWORD}" \
+        "http://localhost:8080/api/v1/dags/${DAG_ID}/dagRuns/${DAG_RUN_ID}" | jq '.state' -r
+}
+
+function wait_for_dag_run {
+    echo "Waiting for DAG RUN"
+    dag_state="running"
+    while [[ "${dag_state}" == "running" ]]; do
+        dag_state=$(fetch_dag_state)
+        echo "Waiting for DAG Run: dag_state=${dag_state}"
+        sleep 1;
+    done;
+}
+
+function test_compose_file() {
+    compose_file="$1"
+    if ! COMPOSE_FILE="${compose_file}" docker-compose config &> "${tmp_output}"; then
+        echo "File unparsable"
+        cat "${tmp_output}"
+        exit 1;
+    fi
+    COMPOSE_FILE="${compose_file}" docker-compose down --volumes --remove-orphans &> /dev/null || true
+
+    if ! COMPOSE_FILE="${compose_file}" docker-compose up -d &> "${tmp_output}"; then
+        echo "All services could not be started"
+        cat "${tmp_output}"
+        exit 1;
+    fi
+
+
+    wait_for_webserver
+    curl -s -X PATCH \
+        --header "Content-Type: application/json" \
+        -u "${AIRFLOW_WWW_USER_USERNAME}:${AIRFLOW_WWW_USER_PASSWORD}" \
+        "http://localhost:8080/api/v1/dags/${DAG_ID}" \
+        --data '{"is_paused": false}'
+
+    curl -s -X POST \
+        --header "Content-Type: application/json" \
+        -u "${AIRFLOW_WWW_USER_USERNAME}:${AIRFLOW_WWW_USER_PASSWORD}" \
+        "http://localhost:8080/api/v1/dags/${DAG_ID}/dagRuns" \
+        --data "$(jq -n '{dag_run_id: $run_id}' --arg 'run_id' "${DAG_RUN_ID}")"
+
+    wait_for_dag_run
+    dag_state=$(fetch_dag_state)
+
+    if [[ "${dag_state}" != "success" ]]; then
+        echo "Test failed"
+        set -x
+        echo "GET http://localhost:8080/api/v1/dags/${DAG_ID}/dagRuns/${DAG_RUN_ID}"
+        curl -X GET \
+            --header "Content-Type: application/json" \
+            -u "${AIRFLOW_WWW_USER_USERNAME}:${AIRFLOW_WWW_USER_PASSWORD}" \
+            "http://localhost:8080/api/v1/dags/${DAG_ID}/dagRuns/${DAG_RUN_ID}"
+        echo "GET http://localhost:8080/api/v1/dags/${DAG_ID}/dagRuns/${DAG_RUN_ID}/taskInstances"
+        curl -X GET \
+            --header "Content-Type: application/json" \
+            -u "${AIRFLOW_WWW_USER_USERNAME}:${AIRFLOW_WWW_USER_PASSWORD}" \
+            "http://localhost:8080/api/v1/dags/${DAG_ID}/dagRuns/${DAG_RUN_ID}/taskInstances"
+        exit 1
+    else
+        echo "Test success"
+    fi
+
+    if ! COMPOSE_FILE="${compose_file}" docker-compose down --volumes &> "${tmp_output}"; then
+        echo "All services could not be stopped"
+        cat "${tmp_output}"
+    fi
+
+}
+#
+find . -name '*.docker-compose.yaml' -type f -maxdepth 1 -print0 | while IFS= read -r -d '' compose_file; do
+    echo "Processing file: ${compose_file}"
+    test_compose_file "$compose_file"
+done
+
+#(
+#    echo "celery-executor--2.1.0--mysql.docker-compose.yaml"
+#    echo "celery-executor--2.1.0--postgres.docker-compose.yaml"
+#    echo "local-executor--2.1.0--mysql.docker-compose.yaml"
+#    echo "local-executor--2.1.0--postgres.docker-compose.yaml"
+#) | while IFS= read -r compose_file; do
+#    echo "Processing file: ${compose_file}"
+#    test_compose_file "$compose_file"
+#done
+
+#compose_file="./celery-executor--2.1.0--mysql.docker-compose.yaml"
+#echo "Processing file: ${compose_file}"
+#test_compose_file "$compose_file"
