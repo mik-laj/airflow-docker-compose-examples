0320c9f451c9512357e51b841acd420332589774
Kamil Breguła <kamilbregula@apache.org>
2021-09-20 05:01:26 +0200

    fixup! fixup! fixup! Update ci.yml

--- scripts/install_compose.sh
+++ scripts/install_compose.sh
@@ -38,6 +38,7 @@ if (( $(ver ${TARGET_VERSION}) >= $(ver "2.0.0") )); then
     echo "exec \"${INSTALL_DIR}/docker-compose-v2\" compose \"\${@}\"" > "${COMPOSE_BIN}"
     echo "${COMPOSE_BIN}"
     cat "${COMPOSE_BIN}"
+    [[ ! -z "$GITHUB_ENV" ]] && (echo "PATH=${INSTALL_DIR}:${PATH}" >> $GITHUB_ENV)
     chmod +x "${COMPOSE_BIN}"
     echo "Installed docker-compose v2"
 else
